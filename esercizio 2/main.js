6d8f1e1dcc9e480c4b9e00e0d774a65457ac967f
Andrea-Lulli <andrea.lulli89@gmail.com>
2022-10-25 14:56:04 +0200

    esercizio 2

---
+++ esercizio 2/main.js
@@ -0,0 +1,29 @@
+
+
+// let numeri = parseInt(prompt("Inseirisci Numero"));
+
+// let stampaPari = document.getElementById("stampapari")
+
+// let stampaDispari = document.getElementById("stampadispari")
+
+
+
+// if (numeri % 2 == 0) {
+
+//     stampaPari.innerHTML = `${numeri} è un numero pari`
+
+// } else if (numeri % 3 == 0) {
+
+//     stampaDispari.innerHTML = `${1 + numeri} è un numero dispari +1`
+// }
+
+
+
+// *Snack2*
+// Generatore di “nomi cognomi” casuali del grande gatsby:
+// Abbiamo una lista di nomi e una lista di cognomi, e da 
+// queste vogliamo generare una "falsa" lista di invitati, 
+// dove ogni invitato nella lista, è l'unione di un "nome" 
+// e un "cognome" delle liste precedentemente dette, scelti 
+// in modo casuale.
+
