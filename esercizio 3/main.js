159b2aee6d18ea15bd9df809522c9977c17bec68
Andrea-Lulli <andrea.lulli89@gmail.com>
2022-10-25 16:47:11 +0200

    esercizio 3 completo

--- esercizio 3/main.js
+++ esercizio 3/main.js
@@ -2,13 +2,17 @@
 // Crea un array di numeri interi e fai la somma di 
 // tutti gli elementi  che sono in posizione (indice) 
 // dispari
-   
+
 let numeri = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
 
-let numeri2 = [0, 1, 2, 3, 4, 5, 6, 7];
+let stampa = document.getElementById("stampa")
+let numeriDispari = 0
 
-let aggiungi =  parseInt(prompt("Aggiungi ilnumero"));
+for (let i = 0; i < numeri.length; i++) {
+if (numeri % 2 != 0) {
 
-if (numeri.length > numeri2.length){
-    console.log(numeri2.push("aggiungi"));
+    numeriDispari = numeriDispari + numeri[i]
+} 
 }
+
+stampa.innerHTML = numeriDispari
