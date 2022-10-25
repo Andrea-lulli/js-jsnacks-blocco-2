25fb86b1aa8df2e3e289417a7b6ab2e5a57c3548
Andrea-Lulli <andrea.lulli89@gmail.com>
2022-10-25 17:28:15 +0200

    modifica esercizio 4

--- esercizio 4/main.js
+++ esercizio 4/main.js
@@ -4,17 +4,19 @@
 // elementi fino a quando ne avrà tanti quanti l’altro.
 
 let numeri = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+console.log();
 
 let numeri2 = [0, 1, 2, 3, 4, 5, 6, 7];
 
-for (let i = 0; i < numeri.length; i++) {
+for (let i = 0; i < 15; i++) {
     let aggiungi = parseInt(prompt("Aggiungi ilnumero"));
 
     if (numeri.length > numeri2.length) {
         console.log(numeri2.push("aggiungi"));
     } else if (numeri.length = numeri2.length) {
         console.log("stesso numero elementi");
-        console.log(numeri2.push("aggiungi"));
-        console.log(numeri.push("aggiungi"));
-    } 
+        console.log(numeri.push("aggiungi"))
+    }
+    console.log(numeri);
+    console.log(numeri2);
 }
